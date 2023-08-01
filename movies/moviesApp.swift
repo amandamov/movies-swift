64c0e5c48132869f8bf1e0d7777f49d0e0d62b50
Amanda <amanda.moov@gmail.com>
2023-08-01 14:05:38 +0200

    Initial Commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by De Oliveira Vasconcelos Amanda Maria on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
